9e1c6ea83a04febb65ffa9b1790d5c269c98a7f4
bl1nkker <nurovich14@gmail.com>
2022-06-10 18:39:00 +0600

    27 Working with Assets

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -10,8 +10,7 @@ void main() {
     ),
     body: const Center(
         child: Image(
-      image: NetworkImage(
-          'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'),
+      image: AssetImage('images/diamond.png'),
     )),
   )));
 }
