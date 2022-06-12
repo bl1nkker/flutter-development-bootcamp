3f0d5b685dc29853768986fa1ecb7fee721ec54a
bl1nkker <nurovich14@gmail.com>
2022-06-12 22:22:20 +0600

    72 Magic 8 Ball - A Decision Making App

--- magic_8_ball_flutter/lib/main.dart
+++ magic_8_ball_flutter/lib/main.dart
@@ -1,7 +1,57 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade800,
+        title: Text('Ask Me Anything'),
+      ),
+      body: SafeArea(
+        child: Container(
+          color: Colors.blue.shade200,
+          child: Ball(),
+        ),
+      ),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  Ball({Key key}) : super(key: key);
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int randomBallAnswer = 1;
+
+  void getRandomBallAnswer() {
+    setState(() {
+      randomBallAnswer = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: GestureDetector(
+          onTap: () {
+            getRandomBallAnswer();
+          },
+          child: Image.asset('images/ball$randomBallAnswer.png')),
+    );
+  }
+}
