1d88a10a59ff34b031d4140f888e6156e4fac445
bl1nkker <nurovich14@gmail.com>
2022-06-12 23:20:14 +0600

    77 How to Play Multiple Sounds

--- xylophone_flutter/lib/main.dart
+++ xylophone_flutter/lib/main.dart
@@ -4,19 +4,84 @@ import 'package:flutter/material.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(String file) {
+    final player = AudioCache();
+    player.play(file);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: GestureDetector(
-              onTap: () {
-                final player = AudioCache();
-                player.play('note4.wav');
-              },
-              child: Text('Click Me'),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Material(
+                color: Colors.red,
+                child: InkWell(
+                  onTap: () {
+                    playSound('note1.wav');
+                  },
+                  child: SizedBox(height: 40),
+                ),
+              ),
+              Material(
+                color: Colors.orange,
+                child: InkWell(
+                  onTap: () {
+                    playSound('note2.wav');
+                  },
+                  child: SizedBox(height: 40),
+                ),
+              ),
+              Material(
+                color: Colors.yellow,
+                child: InkWell(
+                  onTap: () {
+                    playSound('note3.wav');
+                  },
+                  child: SizedBox(height: 40),
+                ),
+              ),
+              Material(
+                color: Colors.green,
+                child: InkWell(
+                  onTap: () {
+                    playSound('note4.wav');
+                  },
+                  child: SizedBox(height: 40),
+                ),
+              ),
+              Material(
+                color: Colors.teal,
+                child: InkWell(
+                  onTap: () {
+                    playSound('note5.wav');
+                  },
+                  child: SizedBox(height: 40),
+                ),
+              ),
+              Material(
+                color: Colors.blue,
+                child: InkWell(
+                  onTap: () {
+                    playSound('note6.wav');
+                  },
+                  child: SizedBox(height: 40),
+                ),
+              ),
+              Material(
+                color: Colors.deepPurple,
+                child: InkWell(
+                  onTap: () {
+                    playSound('note7.wav');
+                  },
+                  child: SizedBox(height: 40),
+                ),
+              ),
+            ],
           ),
         ),
       ),
