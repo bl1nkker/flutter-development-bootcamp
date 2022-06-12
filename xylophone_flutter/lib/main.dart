77a0c85c349e62e46cdea424bcc3ea028210684d
bl1nkker <nurovich14@gmail.com>
2022-06-12 23:07:04 +0600

    75 What are Flutter & Dart Packages?

--- xylophone_flutter/lib/main.dart
+++ xylophone_flutter/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
@@ -8,7 +9,15 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: GestureDetector(
+              onTap: () {
+                final player = AudioCache();
+                player.play('note4.wav');
+              },
+              child: Text('Click Me'),
+            ),
+          ),
         ),
       ),
     );
