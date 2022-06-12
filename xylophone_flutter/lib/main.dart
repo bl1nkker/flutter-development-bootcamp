35fbbacc35ad77baaccb09bcc03baafe06118fe1
bl1nkker <nurovich14@gmail.com>
2022-06-12 23:29:10 +0600

    79 Updating the UI of Our App

--- xylophone_flutter/lib/main.dart
+++ xylophone_flutter/lib/main.dart
@@ -4,85 +4,41 @@ import 'package:flutter/material.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
-  void playSound(String file) {
+  Widget buildKey(Color buttonColor, int soundNumber) {
+    return Expanded(
+      child: Material(
+        color: buttonColor,
+        child: InkWell(
+          onTap: () {
+            playSound(soundNumber);
+          },
+          child: SizedBox(height: 40),
+        ),
+      ),
+    );
+  }
+
+  void playSound(int soundNumber) {
     final player = AudioCache();
-    player.play(file);
+    player.play('note$soundNumber.wav');
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Material(
-                color: Colors.red,
-                child: InkWell(
-                  onTap: () {
-                    playSound('note1.wav');
-                  },
-                  child: SizedBox(height: 40),
-                ),
-              ),
-              Material(
-                color: Colors.orange,
-                child: InkWell(
-                  onTap: () {
-                    playSound('note2.wav');
-                  },
-                  child: SizedBox(height: 40),
-                ),
-              ),
-              Material(
-                color: Colors.yellow,
-                child: InkWell(
-                  onTap: () {
-                    playSound('note3.wav');
-                  },
-                  child: SizedBox(height: 40),
-                ),
-              ),
-              Material(
-                color: Colors.green,
-                child: InkWell(
-                  onTap: () {
-                    playSound('note4.wav');
-                  },
-                  child: SizedBox(height: 40),
-                ),
-              ),
-              Material(
-                color: Colors.teal,
-                child: InkWell(
-                  onTap: () {
-                    playSound('note5.wav');
-                  },
-                  child: SizedBox(height: 40),
-                ),
-              ),
-              Material(
-                color: Colors.blue,
-                child: InkWell(
-                  onTap: () {
-                    playSound('note6.wav');
-                  },
-                  child: SizedBox(height: 40),
-                ),
-              ),
-              Material(
-                color: Colors.deepPurple,
-                child: InkWell(
-                  onTap: () {
-                    playSound('note7.wav');
-                  },
-                  child: SizedBox(height: 40),
-                ),
-              ),
-            ],
-          ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            buildKey(Colors.red, 1),
+            buildKey(Colors.orange, 2),
+            buildKey(Colors.yellow, 3),
+            buildKey(Colors.green, 4),
+            buildKey(Colors.teal, 5),
+            buildKey(Colors.blue, 6),
+            buildKey(Colors.deepPurple, 7),
+          ],
         ),
       ),
     );
