6f847f6f2ddeac7def233feb747ce8eb80322f79
bl1nkker <nurovich14@gmail.com>
2022-06-19 12:44:53 +0600

    123 How to Refactor Flutter Widgets

--- bmi_calculator_flutter/lib/input_page.dart
+++ bmi_calculator_flutter/lib/input_page.dart
@@ -8,13 +8,77 @@ class InputPage extends StatefulWidget {
 class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
+    double defaultPadding = 15.0;
     return Scaffold(
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Padding(
+        padding: EdgeInsets.all(defaultPadding),
+        child: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                      child: ReusableCard(
+                    cardColor: Color(0xFF1D1E33),
+                  )),
+                  SizedBox(
+                    width: defaultPadding * 2,
+                  ),
+                  Expanded(
+                      child: ReusableCard(
+                    cardColor: Color(0xFF1D1E33),
+                  )),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: defaultPadding * 2,
+            ),
+            Expanded(
+                child: ReusableCard(
+              cardColor: Color(0xFF1D1E33),
+            )),
+            SizedBox(
+              height: defaultPadding * 2,
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                      child: ReusableCard(
+                    cardColor: Color(0xFF1D1E33),
+                  )),
+                  SizedBox(
+                    width: defaultPadding * 2,
+                  ),
+                  Expanded(
+                      child: ReusableCard(
+                    cardColor: Color(0xFF1D1E33),
+                  )),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  final Color cardColor;
+  ReusableCard({
+    @required this.cardColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: cardColor, borderRadius: BorderRadius.circular(10.0)),
     );
   }
 }
