f76d5674bf6336f37726df05687118c35b0da14c
bl1nkker <nurovich14@gmail.com>
2022-06-23 16:42:59 +0600

    146 Networking in Flutter Apps with the HTTP Package

--- clima_flutter/lib/screens/loading_screen.dart
+++ clima_flutter/lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:clima_flutter/services/location.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -14,6 +15,16 @@ class _LoadingScreenState extends State<LoadingScreen> {
     getLocation();
   }
 
+  void getData() async {
+    http.Response response = await http.get(Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=7d1666e631b0987960986abde6965cc0'));
+    if (response.statusCode == 200) {
+      print(response.body);
+    } else {
+      print('Error');
+    }
+  }
+
   void getLocation() async {
     Location location = Location();
     await location.getLocation();
@@ -21,6 +32,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
