c0f81e2b3883330561a231be123b3a42c6d1905c
bl1nkker <nurovich14@gmail.com>
2022-06-23 17:27:51 +0600

    152 Refactoring the Location Methods

--- clima_flutter/lib/screens/loading_screen.dart
+++ clima_flutter/lib/screens/loading_screen.dart
@@ -1,10 +1,7 @@
 import 'package:clima_flutter/screens/location_screen.dart';
-import 'package:clima_flutter/services/location.dart';
-import 'package:clima_flutter/services/networking.dart';
+import 'package:clima_flutter/services/weather.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-
-const API_KEY = '7d1666e631b0987960986abde6965cc0';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -12,6 +9,7 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  WeatherModel weatherModel = WeatherModel();
   @override
   void initState() {
     // TODO: implement initState
@@ -20,11 +18,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocationData() async {
-    Location location = Location();
-    await location.getLocation();
-    NetworkHelper networkHelper = NetworkHelper(
-        'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$API_KEY&units=metric');
-    var weatherData = await networkHelper.getData();
+    final weatherData = await weatherModel.getLocationWeather();
     Navigator.push(
         context,
         MaterialPageRoute(
--- clima_flutter/lib/services/weather.dart
+++ clima_flutter/lib/services/weather.dart
@@ -1,4 +1,18 @@
+import 'package:clima_flutter/services/location.dart';
+import 'package:clima_flutter/services/networking.dart';
+
+const API_KEY = '7d1666e631b0987960986abde6965cc0';
+const OPEN_WEATHER_MAP_URL = 'https://api.openweathermap.org/data/2.5/weather';
+
 class WeatherModel {
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getLocation();
+    NetworkHelper networkHelper = NetworkHelper(
+        '$OPEN_WEATHER_MAP_URL?lat=${location.latitude}&lon=${location.longitude}&appid=$API_KEY&units=metric');
+    return await networkHelper.getData();
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
